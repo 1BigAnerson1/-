113004d0e004e55769aedf08c6b3b6eb43c00c26
1BigAnderson1 <sharksharky@yandex.ru>
2023-03-31 16:20:41 +0300

    lesson 8.4.2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import UnoCSS from 'unocss/vite'
 import presetUno from "@unocss/preset-uno";
 import presetIcons  from "@unocss/preset-icons";
+import presetWebFonts from "@unocss/preset-web-fonts";
 export default {
   plugins: [
     UnoCSS({
@@ -9,6 +10,27 @@ export default {
         presetUno(),
         presetIcons({
           /* options */
+        }),
+        presetWebFonts({
+          provider: 'google', // default provider
+          fonts: {
+            // these will extend the default theme
+            sans: "Roboto:400,700",
+            mono: ['Fira Code', 'Fira Mono:400,700'],
+            // custom ones
+            lobster: 'Lobster',
+            lato: [
+              {
+                name: 'Lato',
+                weights: ['400', '700'],
+                italic: true,
+              },
+              {
+                name: 'sans-serif',
+                provider: 'none',
+              },
+            ],
+          },
         }),
 ],  // disable default preset
       rules: [
