8eeffcbc858b9bbdb53c0abe3b410bed5048ab82
1BigAnderson1 <sharksharky@yandex.ru>
2023-04-06 22:00:47 +0300

    06.06 2023 22.00 taski TODO

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import "uno.css";
 import '@unocss/reset/tailwind.css';
+
+const domBtnCreateTask = document.getElementById('btnCreateTask');
+const domBtnCreateTask = document.getElementById('popupCreateTask');
+const domBtnCreateTaskPopup = document.getElementById('popupCreateTaskpopup');
+
+domBtnCreateTask.onclick = (e) => {
+    console.log(e);
+    domBtnCreateTask.classList.remove('hidden');
+
+    const domBtnCloseCreateTaskPopup = document.getElementById("btnCloseCreateTaskPopup");
+     domBtnCloseCreateTaskPopup.onclick = () => {
+        domBtnCreateTask.classList.add("hidden");
+        domBtnCreateCreateTaskPopup.onclick= null;
+    };
+};
