73c06acac9a56b721d5af47bb59885e01afb362a
1BigAnderson1 <sharksharky@yandex.ru>
2023-04-06 20:15:18 +0300

    new day 06.04.2023

--- main.js
+++ main.js
@@ -1,48 +1,54 @@
-const appendBlock = (block) => document.getElementById("app").appendChild(block);
-const randomRange = (min, max) => Mach.floor(Mach.random() * (max - min) + min);
-const generateColor = () => {
+const DOM = (id) => document.getElementById(id);
+const appendBlockToContainer = (block, container) => container.appendChild(block);
+const randomRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const getColorOrEmptyOnRandom = () => {
     const isNotEmpty = Math.random() > 0.5;
     if (isNotEmpty) {
-        return "black"
+        return "black";
     }
-    return null
+    return "red";
 };
-const createBlock = (x, y, size, color) => {
-  const result = document.createElement("div");
-  if (color) {
-      result.style.backgroundColor = color;
-  }
-  console.log(x, y);
+const createBlockWithParamsAtPosition = (x, y, size, color) => {
+    const result = document.createElement("div");
+    if (color) {
+        result.style.backgroundColor = color;
+    }
+    console.log(x, y);
     result.style.width = result.style.height = `${size}px`;
     result.style.position = "absolute";
     result.style.left = `${x}px`;
     result.style.top = `${y}px`;
     return result;
 };
-
 const BLOCK_SIZE = 50;
-const DIMENSION = 5;
-
-let columns = DIMENSION;
-let rows = columns * 2;
+const STEP_DELTA_X = BLOCK_SIZE;
+const DIMENSION = 8;
+const DIMENSION_HALF = DIMENSION / 2;
+const container = DOM("app");
+let columns = 0;
+let rows = 0;
 let xPos = 0;
 let yPos = 0;
+// while (rows-- > 0) {
+let colorsInLine = [];
 
-while (rows-- > 0) {
-    let line = [];
-    xPos = 0;
-    columns = DIMENSION;
-    while (columns-- > 0) {
-        const color = generateColor();
-        const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
-        line.push(color);
-        xPos += BLOCK_SIZE;
-        appendBlock(block);
-    }
-    line.reverse().forEach((color) => {
-        const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
-        appendBlock(block);
-        xPos += BLOCK_SIZE;
-    });
-    yPos += BLOCK_SIZE;
+const saveColorForFutureUse = (color) => {
+ console.log("> saveColorForFutureUse", colorsInLine.length);
+ colorsInLine.push(color);
+};
+
+while (columns < 3) {
+    const color = getColorOrEmptyOnRandom();
+    const block = createBlockWithParamsAtPosition(xPos, yPos, BLOCK_SIZE, color);
+    colorsInLine.push(color);
+    appendBlockToContainer(block, container);
+    xPos += STEP_DELTA_X;
+    columns += 1;
 }
+// const rightHalfOffsetX = (DIMENSION / 2) * BLOCK_SIZE;
+// colorsInLine.reverse().forEach((color, index) => {
+//   const block = createBlock(BLOCK_SIZE * index + rightHalfOffsetX, yPos, BLOCK_SIZE, color);
+//   appendBlockToContainer(block);
+// });
+//   yPos += BLOCK_SIZE;
+// }
