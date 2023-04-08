6662c329540aa2f3e6a9e4a6e7af6469f9b9dcbd
1BigAnderson1 <sharksharky@yandex.ru>
2023-04-08 13:18:43 +0300

    08.04.2023 lesson 13 Satuday

---
+++ src/dom.js
@@ -0,0 +1,11 @@
+const DOM: {
+        Button: {
+            CREATE_TASK: "btnCreateTask",
+            CLOSE_POPUP_CREATE_TASK: "btnCloseCreateTaskPopup",
+        },
+        Popup: {
+            CREATE_TASK: "popupCreateTask",
+        },
+};
+
+export default Keys;
