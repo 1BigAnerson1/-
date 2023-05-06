90a2735e668d81f4b6c0ddf77d6387c6c0de5311
1BigAnderson1 <sharksharky@yandex.ru>
2023-05-06 13:27:17 +0300

    Vue init start

---
+++ src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Enter you name</h1>
+  <div>
+    {{ firstName }} <span
+      style="color: red;"
+      class="card"
+    > {{ lastName }} </span>
+  </div>
+</template>  
+
+<script>
+export default {
+  name: 'MyName',
+  data: () => ({
+    firstName: 'Andrey',
+    lastName: 'Kizya'
+  })
+};
+</script>
+
+<style scoped>
+
+</style>
