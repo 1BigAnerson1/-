01f7773c809c4a93108f7615914c2f02d28389c8
1BigAnderson1 <sharksharky@yandex.ru>
2023-05-18 19:53:44 +0300

    Prodolshaem

--- src/main.js
+++ src/main.js
@@ -1,24 +1,8 @@
-import { createApp } from 'vue';
-import {createRouter, createWebHashHistory} from 'vue-router';
 import './style.css';
+import { createApp } from 'vue';
+import router from './router.js';
+
 import AppComposition from './AppComposition.vue';
-import TodoPage from './components/TodoPage.vue';
-import IndexPage from './components/IndexPage.vue';
-
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes: [
-        {
-            path: '/',
-            component: IndexPage
-        },
-        {
-            path:'/todos',
-            component: TodoPage
-        }
-    ],
-});
-
 
 createApp(AppComposition)
     .use(router)
